a5a2aa390c628998802d428eb058210f72c7abf8
AhmedMuktadir14218 <ahmed15-14218@diu.edu.bd>
2023-08-23 17:59:11 +0600

    first commit

--- .history/tailwind.config_20230823175320.js
+++ .history/tailwind.config_20230823175320.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [require("daisyui")],
 }
+
---
+++ .history/tailwind.config_20230823175356.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  <button className="btn">Button</button>
+<button className="btn btn-neutral">Neutral</button>
+<button className="btn btn-primary">Primary</button>
+<button className="btn btn-secondary">Secondary</button>
+<button className="btn btn-accent">Accent</button>
+<button className="btn btn-ghost">Ghost</button>
+<button className="btn btn-link">Link</button>
+}
+
