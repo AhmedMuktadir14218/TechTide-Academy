a79d33d737ecd5f23d8683881cdddc3cf168bda1
AhmedMuktadir14218 <ahmed15-14218@diu.edu.bd>
2023-08-26 19:09:43 +0600

    add json

---
+++ .history/src/Pages/Shared/Navbar/Navbar_20230826190850.jsx
@@ -0,0 +1,42 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+
+    const navOptions = <>
+    <li><Link to='/'>Home</Link></li>
+    <li><Link to='/courses'>All Courses</Link></li>
+    <li><Link to='/about'>About</Link></li>
+    <li><Link to='/team'>Team</Link></li>
+    <li><Link to='/pricing'>Pricing</Link></li>
+    <li><Link to='/contuct'>Contact Us</Link></li>
+</>
+
+    return (
+        <>
+        <div className="max-w-screen-xl navbar fixed z-10 bg-opacity-30 bg-black text-white">
+<div className="navbar-start">
+ <div className="dropdown">
+   <label tabIndex={0} className="btn   lg:hidden">
+     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+   </label>
+   <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-accent rounded-box w-52">
+     {navOptions}
+   </ul>
+ </div>
+ 
+ <a className="btn btn-ghost normal-case lg:text-xl sm:text-md"><img className="object-contain h-6 w-8 ..." src="/src/assets/react.svg" alt="" /> <Link to="/" >TechTide Academy</Link></a>
+</div>
+<div className="navbar-center hidden lg:flex">
+ <ul className="menu menu-horizontal px-1 font-bold">
+   {navOptions}
+ </ul>
+</div>
+<div className="navbar-end">
+ <a className="btn btn-primary">Enroll</a>
+</div>
+</div>
+     </>
+    );
+};
+
+export default Navbar;
