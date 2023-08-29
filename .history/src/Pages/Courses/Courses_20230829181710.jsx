30c99d6bbe693e81eb6c504019d380afd7869a68
AhmedMuktadir14218 <ahmed15-14218@diu.edu.bd>
2023-08-29 18:20:51 +0600

    404

--- .history/src/Pages/Courses/Courses_20230829181710.jsx
+++ .history/src/Pages/Courses/Courses_20230829181710.jsx
@@ -7,7 +7,7 @@ const Courses = () => {
     return (
         <>
         <CoursesBanner></CoursesBanner>
-             <div className=' grid gap-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-24 mb-8 max-w-screen-xl mx-auto'>
+             <div className=' grid gap-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-24 mb-8 className='max-w-screen-xl mx-auto''>
                 {
                     course.map(option => <CourseList
                         key={option._id} courses={option} 
