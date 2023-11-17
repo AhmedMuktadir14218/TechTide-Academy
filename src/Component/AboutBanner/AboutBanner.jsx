475bc43b453a7557905eda6f95c46e2396c68576
AhmedMuktadir14218 <ahmed15-14218@diu.edu.bd>
2023-11-17 22:05:31 +0600

    course

--- src/Component/AboutBanner/AboutBanner.jsx
+++ src/Component/AboutBanner/AboutBanner.jsx
@@ -9,7 +9,7 @@ const AboutBanner = (props) => {
     <div className="max-w-md">
       <h1 className="mb-5 text-5xl font-bold text-white">{props.title}</h1>
       {/* <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p> */}
-      {/* <button className="btn btn-primary">Get Started</button> */}
+      
     </div>
   </div>
 </div>   
