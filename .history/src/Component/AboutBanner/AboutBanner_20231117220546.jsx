ef7076efcff724bed85a72495f6f29b86d9bb2bb
AhmedMuktadir14218 <ahmed15-14218@diu.edu.bd>
2023-11-17 22:05:54 +0600

    course

--- .history/src/Component/AboutBanner/AboutBanner_20231117220546.jsx
+++ .history/src/Component/AboutBanner/AboutBanner_20231117220546.jsx
@@ -7,7 +7,8 @@ const AboutBanner = (props) => {
   <div className="hero-overlay bg-opacity-60"></div>
   <div className="hero-content text-center text-info-content">
     <div className="max-w-md">
-      <h1 className="mb-5 text-5xl font-bold text-white">{props.title}</h1>    
+      <h1 className="mb-5 text-5xl font-bold text-white">{props.title}</h1>
+          
     </div>
   </div>
 </div>   
