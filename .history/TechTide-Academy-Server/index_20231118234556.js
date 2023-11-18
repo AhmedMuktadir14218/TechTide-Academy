8fc408fd5d9e8235794905ca112d4c1a2c3b6075
AhmedMuktadir14218 <ahmed15-14218@diu.edu.bd>
2023-11-18 23:46:59 +0600

    server

--- .history/TechTide-Academy-Server/index_20231118234556.js
+++ .history/TechTide-Academy-Server/index_20231118234556.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.lo
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
